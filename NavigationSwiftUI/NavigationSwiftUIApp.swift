7ea543c4e6150a1bf0a05fa5fa2acc619af78cef
prongbang <prongbang@gmail.com>
2021-10-09 00:24:27 +0700

    Initial Commit

---
+++ NavigationSwiftUI/NavigationSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationSwiftUIApp.swift
+//  NavigationSwiftUI
+//
+//  Created by Endtry on 9/10/2564 BE.
+//
+
+import SwiftUI
+
+@main
+struct NavigationSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
