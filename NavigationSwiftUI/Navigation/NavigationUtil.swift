36c01cf328d56f133946bff69ee23a9e7bdc6705
prongbang <prongbang@gmail.com>
2022-03-11 21:48:58 +0700

    Add NavigationUtil

---
+++ NavigationSwiftUI/Navigation/NavigationUtil.swift
@@ -0,0 +1,32 @@
+//
+//  NavigationUtil.swift
+//  NavigationSwiftUI
+//
+//  Created by Endtry on 11/3/2565 BE.
+//
+
+import UIKit
+
+struct NavigationUtil {
+    
+    static func popToRootView() {
+        findNavigationController(viewController: UIApplication.shared.windows.filter { $0.isKeyWindow }.first?.rootViewController)?
+            .popToRootViewController(animated: true)
+    }
+    
+    static func findNavigationController(viewController: UIViewController?) -> UINavigationController? {
+        guard let viewController = viewController else {
+            return nil
+        }
+        
+        if let navigationController = viewController as? UINavigationController {
+            return navigationController
+        }
+        
+        for childViewController in viewController.children {
+            return findNavigationController(viewController: childViewController)
+        }
+        
+        return nil
+    }
+}
